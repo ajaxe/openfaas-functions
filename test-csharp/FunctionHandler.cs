73f2836332dfe44a03dbb4b1debe88b9aef0f9c3
Ajay sharma <ajay.sharma1984@gmail.com>
2018-11-25 08:48:05 -0500

    Testing csharp function with private openfaas

---
+++ test-csharp/FunctionHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using Newtonsoft.Json;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        private Dictionary<string, string> headers;
+
+        public FunctionHandler()
+        {
+            headers = new Dictionary<string, string>();
+        }
+        private void BuildHeaderDictionary()
+        {
+            if(!headers.Keys.Any())
+            {
+                foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+                {
+                    if(de.Key.ToString().StartsWith("Http_"))
+                    {
+                        headers.Add(de.Key.ToString(), de.Value?.ToString());
+                    }
+                }
+            }
+        }
+        private string GetHttpMethod()
+        {
+            BuildHeaderDictionary();
+            return headers["Http_Method"];
+        }
+        public string Handle(string input)
+        {
+            var requestMethod = GetHttpMethod();
+            headers.Add("Response_Text", $"[{requestMethod}]Hi there - your input was: {input}");
+            return JsonConvert.SerializeObject(headers);
+        }
+    }
+}
