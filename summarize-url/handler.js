844221998a88e3ea466129b1027edc64341ccb62
Ajay sharma <ajay.sharma1984@gmail.com>
2019-03-02 17:40:59 -0500

    Updating summary data

--- summarize-url/handler.js
+++ summarize-url/handler.js
@@ -1,66 +1,69 @@
-"use strict"
+'use strict'
 
-const https = require("https");
+const https = require('https')
 const url = require('url')
 
-const summaizerUrl = "https://faas.apogee-dev.com/function/summarizer";
-const unfluffUrl = "https://faas.apogee-dev.com/function/unfluff";
+const summaizerUrl = 'https://faas.apogee-dev.com/function/summarizer'
+const unfluffUrl = 'https://faas.apogee-dev.com/function/unfluff'
 
 const httpsPost = function (href, data) {
-    let urlParsed = new url.URL(href)
-    let options = {
-        hostname: urlParsed.hostname,
-        port: urlParsed.port,
-        path: urlParsed.pathname,
-        method: 'POST'
-    }
-    return new Promise(function (resolve, reject) {
-        let req = https.request(options, (res) => {
-            const { statusCode } = res;
-            if (statusCode !== 200) {
-                reject(new Error(`request to url: [${url}] failed. statusCode: ${statusCode}`));
-                return;
-            }
-            res.setEncoding('utf8');
-            let rawData = '';
-            res.on('data', (chunk) => { rawData += chunk; });
-            res.on('end', () => {
-                try {
-                    resolve(rawData);
-                } catch (e) {
-                    reject(e);
-                }
-            });
-        });
-        req.write(data);
-        req.end();
-    });
+  let urlParsed = new url.URL(href)
+  let options = {
+    hostname: urlParsed.hostname,
+    port: urlParsed.port,
+    path: urlParsed.pathname,
+    method: 'POST'
+  }
+  return new Promise(function (resolve, reject) {
+    let req = https.request(options, (res) => {
+      const { statusCode } = res
+      if (statusCode !== 200) {
+        reject(new Error(`request to url: [${url}] failed. statusCode: ${statusCode}`))
+        return
+      }
+      res.setEncoding('utf8')
+      let rawData = ''
+      res.on('data', (chunk) => { rawData += chunk })
+      res.on('end', () => {
+        try {
+          resolve(rawData)
+        } catch (e) {
+          reject(e)
+        }
+      })
+    })
+    req.write(data)
+    req.end()
+  })
 }
 
 const callUnfluff = function (hrefToExtract) {
-    return httpsPost(unfluffUrl, hrefToExtract)
-            .then(function(data){
-                const parsedData = JSON.parse(data);
-                return Promise.resolve(parsedData);
-            });
+  return httpsPost(unfluffUrl, hrefToExtract)
+    .then(function (data) {
+      const parsedData = JSON.parse(data)
+      return Promise.resolve(parsedData)
+    })
 }
 
 const callSummarizer = function (text) {
-    return httpsPost(summaizerUrl, text)
-            .then(function(data){
-                return Promise.resolve(JSON.parse(data));
-            });
+  return httpsPost(summaizerUrl, text)
+    .then(function (data) {
+      return Promise.resolve(JSON.parse(data))
+    })
 }
 
 module.exports = (context, callback) => {
-    callUnfluff(context)
-    .then(function(jsonData){
-        return callSummarizer(jsonData.text);
-    })
-    .then(function(summarized){
-        callback(undefined, { summary: summarized });
-    })
-    .catch(function(e){
-        callback({ error: e.stack }, null);
-    });
+  let c1 = callUnfluff(context)
+  let c2 = c1.then(function (jsonData) {
+    return callSummarizer(jsonData.text)
+  })
+  Promise.all([c1, c2]).then(function (values) {
+    let unfluff = values[0]
+    let summarized = values[1]
+    delete unfluff.text
+    let r = Object.assign({}, unfluff, { summary: summarized })
+    callback(null, r)
+  }).catch(function (e) {
+    callback({ error: e.stack }, null)
+  })
 }
--- unfluff/handler.js
+++ unfluff/handler.js
@@ -1,39 +1,35 @@
-"use strict"
-const extractor = require('unfluff');
-const url = require("url")
-const https = require('https');
-const http = require('http');
-const util = require('util')
+'use strict'
+const extractor = require('unfluff')
+const url = require('url')
+const https = require('https')
+const http = require('http')
 
 module.exports = async (context, callback) => {
-    try {
-        if (!context) {
-            throw new Error("invalid url")
+  try {
+    if (!context) {
+      throw new Error('invalid url')
+    }
+    let link = new url.URL(context)
+    let htmlFetch = link.protocol.toLowerCase() === 'https:' ? https.get : http.get
+    htmlFetch(link.href, function (res) {
+      const { statusCode } = res
+      if (statusCode !== 200) {
+        callback(new Error(`failed to get: ${link.href}, status code: ${statusCode}`), null)
+        return
+      }
+      res.setEncoding('utf8')
+      let rawData = ''
+      res.on('data', (chunk) => { rawData += chunk })
+      res.on('end', () => {
+        try {
+          const extData = extractor(rawData)
+          callback(undefined, extData)
+        } catch (e) {
+          callback(e.stack, null)
         }
-        let link = new url.URL(context)
-        let htmlFetch = link.protocol.toLowerCase() === "https:" ? https.get : http.get;
-        htmlFetch(link.href, function (res) {
-            const { statusCode } = res;
-            const contentType = res.headers['content-type'];
-            if (statusCode !== 200) {
-                callback(`failed to get: ${link.href}, status code: ${statusCode}`, null);
-                return;
-            }
-            res.setEncoding('utf8');
-            let rawData = '';
-            res.on('data', (chunk) => { rawData += chunk; });
-            res.on('end', () => {
-                try {
-                    const extData = extractor(rawData);
-                    callback(undefined, extData);
-                } catch (e) {
-                    callback(err.stack, null);
-                }
-            });
-
-        })
-    }
-    catch (err) {
-        callback(err.stack, null);
-    }
+      })
+    })
+  } catch (err) {
+    callback(err.stack, null)
+  }
 }
