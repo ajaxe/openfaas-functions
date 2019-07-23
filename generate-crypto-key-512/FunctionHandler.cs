61abe8a5805448b4b479ef8a2e034e2064ddee30
Ajay sharma <ajay.sharma1984@gmail.com>
2019-07-22 21:28:35 -0400

    generate crypotgraphically secure 512 random bits

---
+++ generate-crypto-key-512/FunctionHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        private const int KeySize = 512;
+        public string Handle(string input) {
+            using(var svc = RNGCryptoServiceProvider.Create())
+            {
+                var buffer = new byte[KeySize / 8];
+                svc.GetBytes(buffer);
+                return Convert.ToBase64String(buffer);
+            }
+        }
+    }
+}
